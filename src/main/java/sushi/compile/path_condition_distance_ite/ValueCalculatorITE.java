8f3e1347791f8065a899a4e6b8b44b86a027e01b
Giovanni Denaro <denaro@disco.unimib.it>
2026-06-25 13:27:35 +0200

    receive ite operands as functions to avoid early evalution of nested ite operands

--- src/main/java/sushi/compile/path_condition_distance_ite/ValueCalculatorITE.java
+++ src/main/java/sushi/compile/path_condition_distance_ite/ValueCalculatorITE.java
@@ -15,29 +15,32 @@ public abstract class ValueCalculatorITE<VALUE_TYPE, COLOR_TYPE> {
 	 */
 	public abstract double calculate(List<Object> variables);
 
+	@FunctionalInterface
+	public interface IteOperand<V> {
+		V eval();
+	}
+
 	protected VALUE_TYPE ite(COLOR_TYPE colorTrue, COLOR_TYPE colorFalse, 
-			Object r1, Object r2, VALUE_TYPE valTrue, VALUE_TYPE valFalse) {
+			Object r1, Object r2, IteOperand<VALUE_TYPE> valTrue, IteOperand<VALUE_TYPE> valFalse) {
 		if (r1 != null && r1 == r2) {
 			if (colorTrue != null) {
 				color.merge(colorTrue);
 			}
-			return valTrue;
+			return valTrue.eval();
 		} else {
 			if (colorFalse != null) {
 				color.merge(colorFalse);
 			}
-			return valFalse;
+			return valFalse.eval();
 		}
 	}
-
+	
 	protected VALUE_TYPE ite(COLOR_TYPE colorTrue,  
-			Object r1, Object r2, VALUE_TYPE valTrue, VALUE_TYPE valFalse) {
+			Object r1, Object r2, IteOperand<VALUE_TYPE> valTrue, IteOperand<VALUE_TYPE> valFalse) {
 		return ite(colorTrue, null, r1, r2, valTrue, valFalse);
 	}
 
 	public PathConditionColor<COLOR_TYPE> getColor() {
 		return this.color;
-	}
-
-
+	} 
 }
